e78f85ddf1b99b0b10735a5d2e7eaec721899afd
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2019-12-05 22:57:14 +0530

    With Validation

--- addtransaction.py
+++ addtransaction.py
@@ -1,4 +1,7 @@
 blockchain=[]
+blockchain1=[]
+global value2
+value2=False
 global i
 i=1
 global i1
@@ -13,9 +16,33 @@ def block(transaction_amount,last_transaction=[1]):
 def block1(transaction_amount):
     blockchain.append(transaction_amount)
     print(blockchain)
+def validate(blockchain):
+    
+    n=len(blockchain)-1
+    i=0
+    value=False
+    while n>=0:
+        for block in blockchain:
+            
+            i=i+1
+            if i==(n+1):
+                if block[0]==blockchain[n-1]:
+                    value=True
+                else:
+                    print("Invalid blockchain")
+                    value=False
+                    break
+        if value==False:
+            break        
+        n=n-1 
+    if value==True:
+        return True
+    else:
+        return False                   
 while True:
     print("Enter 1:To enter new transaction")
     print("Enter 2:To add transaction")
+    print("Enter 3:To validate the blockchain")
     value=int(input("Enter the value "))
     
     if value==1:
@@ -58,7 +85,27 @@ while True:
         else:
             print("Please Enter a new transaction")
             continue    
-        
+   
+    elif value==3:
+        print(blockchain)
+        blockchain1=blockchain
+        blockchain[0]=[200]
+        value2=validate(blockchain)
+        if value2==True:
+            print("Valid BlockChain")
+            print(blockchain)
+        else:
+            print("invalid Blockchain")
+            blockchain=blockchain1
+            print(blockchain)
+
+
+        continue
+
+
+
+
+
     else:
         print("please enter valid input")
         continue    
