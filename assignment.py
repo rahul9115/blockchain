daab8cb0f058f725667b3c8a3cba09f9fa4e0538
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2019-12-12 13:42:00 +0530

    assignement.py

---
+++ assignment.py
@@ -0,0 +1,25 @@
+#1st part of assignment
+name=input("enter your name: ")
+age=input("enter your age: ")
+#2nd part of assignment
+def display(name,age):
+    concat=name+" "+age
+    print(concat)
+display(name,age)
+#3rd part of assignment
+def display1(x,y):
+    concat=x+y
+    print(concat)
+a="rahul"
+b="vemuri"
+display1(a,b)
+#4th part of assignment  
+def decade(age1):
+    print("Number of decades you have lived: ",int(age1/10))
+age1=int(age)
+decade(age1)
+       
+
+
+
+
