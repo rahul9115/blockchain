16e02e0d5e404613f395a1aee5c5c29467ad0fb9
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-01-23 14:14:20 +0530

    Complete blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,61 @@
-blockchain=[]
-i=0
-def block(blockchain):
-    return blockchain[-1]
-def add(last_transaction=block(blockchain)):
-    global i
-    i=i+1
-    blockchain.append([last_transaction,i])
+blockchain=[[100]]
+value=0
+block=[]
+def transaction():
+    global block
+    value=int(input("Enter the transaction value"))
+    block.append(blockchain[-1])
+    block.append(value)
+    blockchain.append(block)
     print(blockchain)
-add()
-add()
-
-    
+    block=[]
+while True:
+    print("enter 1 to add transaction")
+    print("enter 2 to validate the blockchain")
+    value1=int(input("Enter the value"))
+    if value1==1:
+        transaction()
+        while True:
+            print("do you want to continue y/n")
+            value2=input("Enter the value")
+            if value2=='y':
+                value3=True
+                break
+            elif value2=='n':
+                value3=False
+                break
+            else:
+                print("Enter either y/n")
+                continue
+        if value3==True:
+            continue
+        else:
+            break
+    if value1==2:
+        n=len(blockchain)-1
+        k=0
+        i=0
+        print(n)
+        value4=False
+        while n>0:
+            for blocks in blockchain:
+                
+                if i==n:
+                  
+                    if blocks[0]==blockchain[n-1]:
+                        value4=True
+                    else:
+                        value4=False    
+                else:
+                    i=i+1
+                    continue
+            n=n-1
+            i=0
+        if value4==True:
+            print("Valid Blockchain")
+        else:
+            print("Invalid ")              
+                                
+    else:
+        continue
+            
